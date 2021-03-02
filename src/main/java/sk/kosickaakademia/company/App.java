25183fbb92c3ef317b068cfd2e2bc95517145b59
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-03-02 09:51:27 +0100

    Initial commit

---
+++ src/main/java/sk/kosickaakademia/company/App.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
